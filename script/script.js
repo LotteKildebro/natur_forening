b4c1f74b50e16d6e1c85012b115deaaf8a32cea2
LotteKildebro <4514756@RTS-365.dk>
2020-04-28 22:54:48 +0200

    nu

--- script/script.js
+++ script/script.js
@@ -24,3 +24,71 @@ btn.addEventListener('click', () => window.scrollTo({
     top: 800,
     behavior: 'smooth',
 }));
+
+var button
+
+button = document.getElementById('submit');
+button.addEventListener('click', validateForm);
+
+// form validatation 
+function validateForm() {
+
+    event.preventDefault();
+    if (document.myForm.penge.value == "") {
+        alert("indtast beløb ;-) tak");
+        document.myForm.penge.focus();
+        return false;
+    }
+    if (document.myForm.email.value == "") {
+        msg1.innerHTML = 'angiv email';
+        document.myForm.email.focus();
+        return false;
+    }
+    if (document.myForm.name.value == "") {
+        msg2.innerHTML = 'angiv navn';
+        document.myForm.name.focus();
+        return false;
+    }
+
+    if (document.myForm.kort.value == "") {
+        alert("Ikke godkendt kortnummer!");
+        document.myForm.email.focus();
+        return false;
+    }
+    if (document.myForm.cvr.value == "") {
+        alert("Ikke godkendt cvr!");
+        document.myForm.cvr.focus();
+        return false;
+    }
+    if (document.myForm.dato.value == "") {
+        alert("Ikke godkendt udløbsdato!");
+        document.myForm.dato.focus();
+        return false;
+    }
+    confirm("Tak for donationen!");
+    window.location.reload();
+    return (true);
+
+
+
+}
+function maxLength() {
+    if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);
+    type = "number"
+    maxlength = "3"
+};
+
+
+
+function validateEmail() {
+    var emailID = document.myForm.email.value;
+    atpos = emailID.indexOf("@");
+    dotpos = emailID.lastIndexOf(".");
+
+    if (atpos < 1 || (dotpos - atpos < 2)) {
+        alert("Please enter correct email ID")
+        document.myForm.email.focus();
+        return false;
+    }
+    return (true);
+}
